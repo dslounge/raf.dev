3da90e0b27364d3e884f5cbf80aa53337b646fe0
Rafael Mendiola <dslounge@gmail.com>
2019-07-25 17:01:00 -0400

    progress

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,38 +7,30 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import SEO from "./seo"
 
-import Header from "./header"
-import "./layout.css"
+const styles = {
+  body: {
+    fontFamily: "Helvetica, sans-serif",
+    margin: `0 auto`,
+    maxWidth: 960,
+    padding: `0px 1.0875rem 1.45rem`,
+    paddingTop: 0,
+    backgroundColor: "red",
+  },
+}
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = ({ children, title }) => {
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
+      <div style={styles.body}>
+        <main>
+          <SEO title={title} />
+          {children}
+        </main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          {"©"}
+          {new Date().getFullYear()}
         </footer>
       </div>
     </>
@@ -47,6 +39,7 @@ const Layout = ({ children }) => {
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,63 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+// import Image from "../components/image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+
+// <Link to="/page-2/">Go to page 2</Link>
+
+// <Image />
+
+const spacing = {
+  TINY: "6px",
+  SMALL: "12px",
+  BASE: "24px",
+}
+
+const colors = {
+  black: "#353535",
+  gray: "#999999",
+  white: "#ffffff",
+}
+
+const styles = {
+  row: {
+    backgroundColor: "rgba(0, 0, 0, .3)",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  socialButton: {
+    backgroundColor: "rgba(0, 0, 0, .4)",
+    padding: spacing.SMALL,
+    margin: spacing.SMALL,
+  },
+  name: {
+    backgroundColor: colors.black,
+    fontSize: 65,
+    color: colors.white,
+    padding: spacing.SMALL,
+    margin: spacing.SMALL,
+  },
+  rafTitle: {
+    backgroundColor: colors.gray,
+    fontSize: 40,
+    color: colors.white,
+    padding: spacing.SMALL,
+    margin: spacing.SMALL,
+  },
+}
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <Layout title="Home">
+    <div style={styles.name}>Rafael Mendiola</div>
+    <div style={styles.rafTitle}>Software Developer</div>
+    <div style={styles.row}>
+      <div style={styles.socialButton}>Email</div>
+      <div style={styles.socialButton}>Twitter</div>
+      <div style={styles.socialButton}>Instagram</div>
+      <div style={styles.socialButton}>LinkedIn</div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
