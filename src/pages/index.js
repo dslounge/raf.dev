719c70bd665d8a464192428f8e44c5dc2374d152
Rafael Mendiola <dslounge@gmail.com>
2019-07-26 00:27:50 -0400

    progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,27 +23,44 @@ const styles = {
   },
   profileImage: {
     width: 200,
+    position: "relative",
+    zIndex: 2,
   },
   name: {
     display: "inline-block",
     backgroundColor: colors.black,
     fontSize: 50,
+    lineHeight: "50px",
     color: colors.white,
     padding: spacing.TINY,
     borderRadius: "10px",
     fontStyle: "italic",
     transform: "rotate(-9deg)",
-    paddingLeft: 100,
+    paddingLeft: 170,
+    paddingRight: 20,
   },
   rafTitle: {
     display: "inline-block",
     backgroundColor: colors.gray,
     fontSize: 30,
+    lineHeight: "25px",
     color: colors.white,
     padding: spacing.TINY,
     borderRadius: "10px",
     transform: "rotate(-5deg)",
-    paddingLeft: 100,
+    marginTop: 10,
+    paddingLeft: 170,
+    paddingRight: 20,
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerInner: {
+    position: "relative",
+    height: 350,
+    width: 600,
   },
 };
 
@@ -51,10 +68,21 @@ const styles = {
 
 const IndexPage = () => (
   <Layout title="Home">
-    <img style={styles.profileImage} src={rafProfile} alt="raf profile" />
-    <div>
-      <div style={styles.name}>Rafael Mendiola</div>
-      <div style={styles.rafTitle}>Software Developer</div>
+    <div style={styles.header}>
+      <div style={styles.headerInner}>
+        <img style={styles.profileImage} src={rafProfile} alt="raf profile" />
+        <div
+          style={{
+            transform: "translate(40px, -210px)",
+            position: "relative",
+            zIndex: 1,
+          }}
+        >
+          <div style={styles.name}>Rafael Mendiola</div>
+          <br />
+          <div style={styles.rafTitle}>Software Developer</div>
+        </div>
+      </div>
     </div>
     <div style={styles.row}>
       <SocialButton
