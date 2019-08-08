28de29418fa68e78e950e9baa963eed3998ae17d
Rafael Mendiola <dslounge@gmail.com>
2019-08-08 15:41:25 -0400

    fade in and fade out

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+/* eslint-disable jsx-a11y/media-has-caption */
+import React, { useState, useRef } from "react";
 import { PropTypes } from "prop-types";
 import "./style.scss";
 
 export const Project = props => {
   const [showVideo, setShowVideo] = useState(false);
-
   const { image, video, title, url } = props;
+  const videoStyle = showVideo ? "videoShow" : "videoHide";
+
+  const videoRef = useRef();
+
   return (
     <a
       href={url}
       onMouseEnter={() => {
+        videoRef.current.play();
         setShowVideo(true);
       }}
       onMouseLeave={() => {
+        videoRef.current.pause();
         setShowVideo(false);
       }}
     >
       <div className="project">
-        <img src={image} />
-        {showVideo && <video src={video} type="video/mp4" autoPlay />}
+        <img src={image} alt="project preview" />
+        <video
+          ref={videoRef}
+          className={videoStyle}
+          src={video}
+          type="video/mp4"
+          loop
+        />
         <p>{title}</p>
       </div>
     </a>
